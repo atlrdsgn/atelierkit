1e5c4ed0ab62b4b4cb54adbe0401d1af435d6421
chvndler <chvndler.ch@icloud.com>
2023-04-03 00:50:06 -0400

    in

--- package/atelierkit.pkg/src/components/input/input.css.ts
+++ package/atelierkit.pkg/src/components/input/input.css.ts
@@ -3,6 +3,13 @@
 import {style, styleVariants} from '@vanilla-extract/css'
 import {recipe, RecipeVariants} from '@vanilla-extract/recipes'
 
+const property_vars = {
+  radii: {
+    SMALL: 14,
+    MEDIUM: 17,
+  },
+} as const
+
 export const input_root = style({
   /**
    *
@@ -29,4 +36,34 @@ export const input_root = style({
 
 export const input_label = style({})
 
-export const input_field = recipe({})
+const input_base = style({
+  all: 'unset',
+  boxSizing: 'border-box',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  alignContent: 'center',
+
+  paddingTop: 8,
+  paddingBottom: 8,
+  paddingLeft: 16,
+  paddingRight: 16,
+  gap: 10,
+
+  width: '100%',
+  height: 'auto',
+  minWidth: '400px',
+
+  border: `2px solid #FFDAFC`,
+  borderRadius: property_vars.radii.MEDIUM,
+})
+
+export const input_field = recipe({
+  /**
+   *
+   * the input field.
+   */
+  base: [input_base],
+  variants: {},
+  defaultVariants: {},
+})
--- package/atelierkit.pkg/src/lib/theme.css.ts
+++ package/atelierkit.pkg/src/lib/theme.css.ts
@@ -1,7 +1,6 @@
 /** @format */
 
 import {kitColorPalette} from './color'
-import {themeVars} from './vars.css'
 
 import type {ColorTypes} from './vars.css'
 
@@ -13,24 +12,111 @@ export const theme = {
     xlarge: '1200px',
   },
 
+  /** -------------------------------------- */
   font: {
     family: {
-      system: `"Inter", sans-serif`,
+      system: `'Inter', sans-serif`,
+      mono: `'SF Mono', monospace`,
     },
-    heading: {},
+    heading: {
+      H1: '2.25rem', // 36px
+      H2: '1.875rem', // 30px
+      H3: '1.5rem', // 24px
+      H4: '1.25rem', // 20px
+      H5: '1.125rem', // 18px
+      H6: '1rem', // 16px
+    },
     body: {},
-    size: {},
-    weight: {},
+    size: {
+      XS: '0.75rem', // 12px
+      SM: '0.875rem', // 14px
+      MD: '1rem', // 16px
+      LG: '1.125rem', // 18px
+      XL: '1.25rem', // 20px
+      XXL: '1.5rem', // 24px
+      '3XL': '1.875rem', // 30px
+      '4XL': '2.25rem', // 36px
+      '5XL': '3rem', // 48px
+      '6XL': '3.75rem', // 60px
+      '7XL': '4.5rem', // 72px
+      '8XL': '6rem', // 96px
+      '9XL': '8rem', // 128px
+    },
+    weight: {
+      SUPRLITE: '100',
+      ULTRALITE: '200',
+      LITE: '300',
+      REGULAR: '400',
+      MEDIUM: '500',
+      SEMIBOLD: '600',
+      BOLD: '700',
+      HEAVY: '800',
+      BLACK: '900',
+    },
   },
 
-  radii: {},
-  space: {},
+  /** ---------------------------------------- */
 
+  radii: {
+    NO: '0px',
+    DF: '14px',
+    XS: '6px',
+    SM: '12px',
+    MD: '14px',
+    LG: '16px',
+    XL: '18px',
+    XXL: '20px',
+    PILL: '1000px',
+  },
+  space: {
+    ZERO: '0px',
+    NO: '0px',
+    DF: '16px', // IPX
+    APX: '1px',
+    BPX: '2px',
+    CPX: '0.25rem', // 4px
+    DPX: '0.375rem', // 6px
+    EPX: '0.5rem', // 8px
+    FPX: '0.625rem', // 10px
+    GPX: '0.75rem', // 12px
+    HPX: '0.875rem', // 14px
+    IPX: '1rem', // 16px
+    JPX: '1.125rem', // 18px
+    KPX: '1.25rem', // 20px
+    LPX: '1.5rem', // 24px
+    MPX: '1.75rem', // 28px
+    NPX: '2rem', // 32px
+    OPX: '2.25rem', // 36px
+    PPX: '2.5rem', // 40px
+    QPX: '2.75rem', // 44px
+    RPX: '3rem', // 48px
+    SPX: '3.5rem', // 56px
+    TPX: '4rem', // 64px
+    UPX: '4.5rem', // 72px
+    VPX: '6rem', // 96px
+    WPX: '8rem', // 128px
+    XPX: '12rem', // 192px
+    YPX: '15rem', // 240px
+    ZPX: '20rem', // 320px
+  },
   z: {
-    indice: {},
+    indice: {
+      ZERO: '0',
+      DF: '1',
+      LOW: '2',
+      MED: '10',
+      HIGH: '30',
+      TOP: '100',
+      MAX: '99999',
+    },
   },
-
-  shadow: {},
+  shadow: {
+    NO: 'none',
+    DF: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+    LOW: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+    MED: '0px 8px 16px rgba(0, 0, 0, 0.1)',
+    HIGH: '0px 12px 24px rgba(0, 0, 0, 0.1)',
+  },
 
   // ..
 } as const // <--- this is the important part
