4904f1f70ba283e8b0b7a1dfd38a8c82200766bf
chvndler <chvndler.ch@icloud.com>
2023-05-01 22:16:10 -0400

    changeset

--- package/eslint-config-atlrdsgn/index.js
+++ package/eslint-config-atlrdsgn/index.js
@@ -1,33 +1,33 @@
 module.exports = {
-  extends: [
-    'next',
-    'turbo',
-    'prettier',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  rules: {},
+    extends: [
+        'next',
+        'turbo',
+        'prettier',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {},
 
-  /**
-   *
-   * see: 'https://typescript-eslint.io/getting-started/'
-   * for more information on set-up.
-   */
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    /**
+     *
+     * see: 'https://typescript-eslint.io/getting-started/'
+     * for more information on set-up.
+     */
+    settings: {
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+            typescript: {
+                alwaysTryTypes: true,
+                project: '.',
+            },
+        },
+        react: {
+            version: 'detect',
+        },
     },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: '.',
-      },
-    },
-    react: {
-      version: 'detect',
-    },
-  },
 };
