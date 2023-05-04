39891b0907192408008a48b1e029e19da9bd7eb7
chvndler <chvndler.ch@icloud.com>
2023-05-04 18:26:17 -0400

    lint

--- package/eslint-config-atlrdsgn/index.js
+++ package/eslint-config-atlrdsgn/index.js
@@ -9,6 +9,9 @@ module.exports = {
     ],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
+    parserOptions: {
+        project: ['../atlrdsgn-tsconfig/tsconfig.eslint.json'],
+    },
     rules: {},
 
     /**
