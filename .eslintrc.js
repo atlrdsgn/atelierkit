c40738297144c27183115e77a20a0dabcdfe5831
chvndler <chvndler.ch@icloud.com>
2023-06-27 21:28:06 -0400

    sd

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
     root: true,
     // This tells ESLint to load the config from the package `eslint-config-acme`
     extends: ['atlrdsgn'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      tsconfigRootDir: __dirname,
+      project: ['./package/*/tsconfig.json'],
+    },
     settings: {
         next: {
             rootDir: ['apps/*/'],
