82e540faf4b2d2e5c70d1e1878b6a61d83acf1cc
chvndler <chvndler.ch@icloud.com>
2023-07-03 20:32:04 -0400

    remove - next-kit

--- apps/next-kit/src/components/react-kit/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './DemoButton';
-export * from './DemoChip';
-export * from './DemoText';
-export * from './DemoParagraph';
--- apps/next-kit/src/components/theme-provider.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { KitProvider } from '@atlrdsgn/kit';
-
-const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  return <KitProvider>{children}</KitProvider>;
-};
-
-export default ThemeProvider;
